21ccc0a5dfb57741a466bb68c900920f73aaa806
kristoffer <kristoffer@disputas.no>
2020-08-18 17:44:38 +0200

    rm webtek folder

--- IT2805_Webtek/Assignment_6/script.js
+++
@@ -1,5 +0,0 @@
-function beregnTax() {
-    const income= document.getElementById("income").value;
-    const wealth=document.getElementById("wealth").value;
-    document.getElementById("tax").value=((0.35 * income) + (0.25 * wealth));
-}
